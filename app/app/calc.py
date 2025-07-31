80240fa4aa1204af9db4e1b89871a4442e92a1a9
JperezRi03 <juan.perez@lgcns.com>
2025-07-31 11:34:30 -0500

    Configured Django

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator Functions.
+"""
+def add(a, b):
+    """Add two numbers together."""
+    return a + b
+
+def substract(a, b):
+    """Add two numbers together."""
+    return a - b
---
+++ app/app/test.py
@@ -0,0 +1,14 @@
+"""
+Sample Test
+"""
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.substract(10 , 5)
+        self.assertEqual(res, 5)
