17df400658f67da551c8f03ba029bbb3f1bc01e3
JperezRi03 <juan.perez@lgcns.com>
2025-07-31 16:36:43 -0500

    Create a new project to test conection whit DB

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from nt import environ
 import os
 from pathlib import Path
 
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
