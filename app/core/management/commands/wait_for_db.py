91fd421e8f5506477ded7f88b49f6205e25e044e
JperezRi03 <juan.perez@lgcns.com>
2025-08-04 15:14:49 -0500

    Model User and migrations created

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,8 +6,7 @@ import time
 from django.core.management.base import BaseCommand
 
 from django.db.utils import OperationalError
-from django.core.management.base import BaseCommand
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error 
 
 
 class Command(BaseCommand):
