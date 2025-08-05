b02dddf6753ae7dd6d5961c53b9998dfdf04cf31
JperezRi03 <juan.perez@lgcns.com>
2025-08-05 17:44:16 -0500

    Make Tests to users model and SuperUserModel

--- app/core/models.py
+++ app/core/models.py
@@ -7,11 +7,26 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address')
+
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin): # pyright: ignore[reportIncompatibleVariableOverride]
     email = models.EmailField(max_length= 255, unique=True)
