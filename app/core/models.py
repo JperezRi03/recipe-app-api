8d0f753b7c5ba97060e746044a78cd294d9e07fa
JperezRi03 <juan.perez@lgcns.com>
2025-08-06 17:39:13 -0500

    Finish Admin Setup

--- app/core/models.py
+++ app/core/models.py
@@ -16,17 +16,12 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, password, **extra_fields)
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin): # pyright: ignore[reportIncompatibleVariableOverride]
     email = models.EmailField(max_length= 255, unique=True)
